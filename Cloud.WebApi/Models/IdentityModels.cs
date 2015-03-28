baa3e53f471cb0531659a0228d87fd694586cae4
iobodinsky <obodian.ru@mail.ru>
2015-03-29 01:14:16 +0200

    Added CloudDbEntities context from CloudDb, changed connection strings

--- Cloud.WebApi/Models/IdentityModels.cs
+++ Cloud.WebApi/Models/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace Cloud.WebApi.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("CloudDbEntities", throwIfV1Schema: false)
         {
         }
         
