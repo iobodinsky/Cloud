dad729519db41d8433e01ed957339dcaef0ca816
iobodinsky <obodian.ru@mail.ru>
2015-03-29 01:22:27 +0200

    Added RepositoryBase and UserFileRepository

---
+++ Cloud.Repositories/Repositories/UserFileRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cloud.Repositories.DataContext;
+
+namespace Cloud.Repositories.Repositories
+{
+    public class UserFileRepository : RepositoryBase
+    {
+        public IEnumerable<UserFile> GetAllFiles(string userId)
+        {
+            return Entities.UserFiles.Where(file => file.UserId == userId);
+        }
+
+        public void Delete(int fileId)
+        {
+            var fileToDelete = Entities.UserFiles.First(file => file.FileId == fileId);
+            if (fileToDelete == null) return;
+
+            // Delete file from storage first
+            var filePath = Path.Combine(fileToDelete.Path, fileToDelete.Name);
+            File.Delete(filePath);
+
+            // Delete file from db
+            Entities.UserFiles.Attach(fileToDelete);
+            Entities.UserFiles.Remove(fileToDelete);
+            Entities.SaveChanges();
+        }
+    }
+}
---
+++ Cloud.Repositories/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using Cloud.Repositories.DataContext;
+
+namespace Cloud.Repositories
+{
+     /// <summary>
+    ///    Base repository class
+    /// </summary>
+    public abstract class RepositoryBase
+    {
+        protected RepositoryBase()
+        {
+            Entities = new CloudDbEntities();
+            Entities.Configuration.LazyLoadingEnabled = false;
+        }
+
+        /// <summary>
+        ///    Access Cloud entities
+        /// </summary>
+        public CloudDbEntities Entities { get; private set; }
+
+        /// <summary>
+        ///    Save context changes to the database
+        /// </summary>
+        public virtual void SaveChanges()
+        {
+            Entities.SaveChanges();
+        }
+
+        /// <summary>
+        ///    Generic method to add entity to the data context
+        /// </summary>
+        /// <param name="entity">The entity that has to be added to the data context</param>
+        /// <param name="isAutoSave">If AutoSave is true the entity will utomatically be saved to the database</param>
+        /// <typeparam name="T">Entity type</typeparam>
+        public void Add<T>(T entity, bool isAutoSave) where T : class
+        {
+            var dbSet = Entities.Set<T>();
+            if (dbSet == null) return;
+            dbSet.Add(entity);
+            if (isAutoSave)
+            {
+                SaveChanges();
+            }
+        }
+    }
+}
