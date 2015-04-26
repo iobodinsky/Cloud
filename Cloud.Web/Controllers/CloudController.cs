2e5b44d4db6e2aa99dbfccd60722ada87ecca21e
iobodinsky <obodian.ru@mail.ru>
2015-04-27 02:00:12 +0300

    Added Cloud controller styles, scripts, layout view to Web

---
+++ Cloud.Web/Controllers/CloudController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Cloud.Web.Controllers
+{
+    public class CloudController : Controller
+    {
+        // GET: Cloud
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
