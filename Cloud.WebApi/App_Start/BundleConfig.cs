c95b1f7fd2f35f3e4bf4e31dce0c4748b3d51efe
iobodinsky <obodian.ru@mail.ru>
2015-04-29 01:23:20 +0300

    Updated packages, configure bundles

---
+++ Cloud.WebApi/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Optimization;
+
+namespace Cloud.WebApi
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/common").Include(
+                "~/Scripts/assets/js/jquery-1.10.2.min.js",
+                "~/Scripts/assets/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ng").Include(
+                "~/Scripts/assets/js/angular.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .IncludeDirectory("~/Content", "*.css", true));
+
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
