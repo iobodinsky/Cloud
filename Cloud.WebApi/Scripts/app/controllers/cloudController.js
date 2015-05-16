cf0ab22eca8c19cca8cab036cd5eb13859028488
ivan-obodianskyi <ivan.obodinaskyi@neadevis.com>
2015-05-16 15:17:52 +0300

    Refactor cloudController

--- Cloud.WebApi/Scripts/app/controllers/cloudController.js
+++ Cloud.WebApi/Scripts/app/controllers/cloudController.js
@@ -2,154 +2,134 @@
 
 cloud.controllers = cloud.controllers || {};
 
-cloud.controllers.cloudController = cloud.controllers.cloudController || function ($scope, $http, $window, fileUploader) {
-    var self = this;
+cloud.controllers.cloudController = cloud.controllers.cloudController ||
+	function($scope, $http, $window, fileUploader) {
+		var self = this;
 
-    self.getToken = function() {
-        return $window.sessionStorage.getItem(cloud.models.constants.userTokenKey) || '';
-    };
+		self.init = function () {
+			$scope.folders = [];
+			$scope.files = [];
 
-    self.updateFiles = function () {
-        var token = self.getToken();
+			var token = self.getToken();
+			if (token) {
+				self.getUserInfo(token);
+				self.getFiles(token);
+			} else {
+				$scope.isLogin = true;
+			}
+		};
+		self.getToken = function () {
+			return $window.sessionStorage.getItem(cloud.models.constants.userTokenKey) || '';
+		};
+		self.getUserInfo = function (token) {
+			var userInfoRequest = {
+				method: 'GET',
+				url: cloud.models.constants.urls.userInfo,
+				headers: {
+					'Authorization': 'Bearer ' + token
+				}
+			};
+			$http(userInfoRequest)
+				.success(function (data, status, headers, config) {
+					$scope.userName = data.Email;
+				})
+				.error(function (data, status, headers, config) {
 
-        var filesFoldersRequest = {
-            method: 'GET',
-            url: cloud.models.constants.urls.files,
-            headers: {
-                'Authorization': 'Bearer ' + token
-            }
-        };
+				});
+		};
+		self.getFiles = function (token) {
+			var filesFoldersRequest = {
+				method: 'GET',
+				url: cloud.models.constants.urls.files,
+				headers: {
+					'Authorization': 'Bearer ' + token
+				}
+			};
 
-        $http(filesFoldersRequest)
-            .success(function (data, status, headers, config) {
-                $scope.folders = data.Folders;
-                $scope.files = data.Files;
-            })
-            .error(function (data, status, headers, config) {
+			$http(filesFoldersRequest)
+				.success(function (data, status, headers, config) {
+					for (var i = 0; i < data.Folders.length; i++) {
+						$scope.folders.push(data.Folders[i]);
+					}
 
-            });
-    };
+					for (var j = 0; j < data.Files.length; j++) {
+						$scope.files.push(data.Files[j]);
+					}
+				})
+				.error(function (data, status, headers, config) {
 
-    $scope.uploader = new fileUploader();
-    $scope.uploader.url = cloud.models.constants.urls.upload;
-    $scope.uploader.headers = {
-        'Authorization': 'Bearer ' + self.getToken()
-    };
+				});
+		};
 
-    $scope.uploader.onCompleteItem = function (item, response, status, headers) {
-        $scope.files.push(item.file.name);
+		$scope.uploader = new fileUploader();
+		$scope.uploader.url = cloud.models.constants.urls.upload;
+		$scope.uploader.headers = {
+			'Authorization': 'Bearer ' + self.getToken()
+		};
 
-        self.updateFiles();
-    }
+		$scope.uploader.onCompleteItem =
+			function(uploadedItem, response, status, headers) {
+				$scope.files.push(uploadedItem.file);
+			}
 
-    $scope.isCloud = true;
+		$scope.isCloud = true;
 
-    $scope.register = function () {
-        var registrationData = {
-            Email: this.userRegistrationEmail,
-            Password: this.userRegistrationPassword,
-            ConfirmPassword: this.userRegistrationConfirmPassword
-        };
+		$scope.register = function() {
+			var registrationData = {
+				Email: this.userRegistrationEmail,
+				Password: this.userRegistrationPassword,
+				ConfirmPassword: this.userRegistrationConfirmPassword
+			};
 
-        var registerRequest = {
-            method: 'POST',
-            url: cloud.models.constants.urls.register,
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(registrationData)
-        };
+			var registerRequest = {
+				method: 'POST',
+				url: cloud.models.constants.urls.register,
+				contentType: 'application/json; charset=utf-8',
+				data: JSON.stringify(registrationData)
+			};
 
-        $http(registerRequest)
-            .success(function (data, status, headers, config) {
-                
-            })
-            .error(function (data, status, headers, config) {
+			$http(registerRequest)
+				.success(function(data, status, headers, config) {
 
-            });
-    };
+				})
+				.error(function(data, status, headers, config) {
 
-    $scope.login = function () {
-        var userLogin = this.userLogin;
-        var userPassword = this.userPassword;
+				});
+		};
 
-        var loginData = {
-            grant_type: 'password',
-            username: userLogin,
-            password: userPassword
-        }
+		$scope.login = function() {
+			var userLogin = this.userLogin;
+			var userPassword = this.userPassword;
 
-        var loginRequest = {
-            method: 'POST',
-            url: cloud.models.constants.urls.token,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: 'grant_type=password&username=' + loginData.username + '&password=' + loginData.password
+			var loginData = {
+				grant_type: 'password',
+				username: userLogin,
+				password: userPassword
+			}
 
-        };
+			var loginRequest = {
+				method: 'POST',
+				url: cloud.models.constants.urls.token,
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				data: 'grant_type=password&username=' + loginData.username + '&password=' + loginData.password
 
-        $http(loginRequest)
-            .success(function(data, status, headers, config) {
-                $window.sessionStorage.setItem(
-                    cloud.models.constants.userTokenKey, data.access_token);
-                $scope.isLogin = false;
-                self.init();
-            })
-            .error(function(data, status, headers, config) {
+			};
 
-            });
-    }
+			$http(loginRequest)
+				.success(function(data, status, headers, config) {
+					$window.sessionStorage.setItem(
+						cloud.models.constants.userTokenKey, data.access_token);
+					$scope.isLogin = false;
+					self.init();
+				})
+				.error(function(data, status, headers, config) {
 
-    $scope.submitFile = function () {
-        var formData = new FormData();
-        //Take the first selected file
-        //formData.append("uploadedFile", files[0]);
-        var s = $scope.uploadedFile;
+				});
+		}
 
-        $http.post(cloud.models.constants.urls.upload, s, {
-            withCredentials: true,
-            headers: {
-                'Authorization': 'Bearer ' + self.getToken()
-            },
-            transformRequest: angular.identity
-        }).success(function(data) {
-
-            }).error(function(data) {
-
-            }
-        );
-    };
-
-    $scope.folders = [];
-    $scope.files = [];
-
-    // Private
-    self.init = function() {
-        var token = self.getToken();
-
-        if (token) {
-            var userInfoRequest = {
-                method: 'GET',
-                url: cloud.models.constants.urls.userInfo,
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                }
-            };
-            $http(userInfoRequest)
-                .success(function (data, status, headers, config) {
-                    $scope.userName = data.Email;
-                })
-                .error(function (data, status, headers, config) {
-
-                });
-
-            self.updateFiles();
-
-        } else {
-            $scope.isLogin = true;
-        }
-    };
-
-    self.init();
-
-    
-};
+		// Private
+		
+		self.init();
+	};
--- Cloud.WebApi/Scripts/app/models/constants.js
+++ Cloud.WebApi/Scripts/app/models/constants.js
@@ -11,5 +11,5 @@ cloud.models.constants.urls.token = '/Token';
 cloud.models.constants.urls.register = 'api/Account/Register';
 cloud.models.constants.urls.userInfo = 'api/Account/UserInfo';
 cloud.models.constants.urls.files = 'api/files';
-cloud.models.constants.urls.upload = 'api/files/cloud/2/folder/906ffdae-8c6d-43aa-a7e0-2a25a868bef9/upload';
+cloud.models.constants.urls.upload = 'api/files/cloud/2/folder/baba2553-f024-4afb-aa8d-358b9e1ebf4a/upload';
 cloud.models.constants.urls.driveUpload = 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart';
